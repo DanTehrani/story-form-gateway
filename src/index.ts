f5deab274eb0762a4f2123bf30751d58327ddb14
Daniel Tehrani <contact@dantehrani.com>
2022-09-19 16:30:47 +0900

    Setup Sentry

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,39 @@ import "dotenv/config";
 import express from "express";
 import * as form from "./lib/form";
 import cors from "cors";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { CreateFormRequest, SubmitAnswerRequest } from "./types";
 
 const { PORT } = process.env;
 
 const app: express.Application = express();
 
+Sentry.init({
+  dsn: "https://1b3b81a1c60d4561ab1e968719db5f0a@o1348995.ingest.sentry.io/6758756",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app })
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+});
+
 const port: number = parseInt(PORT) || 4000;
 
 app.use(express.json());
 app.use(cors());
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.get("/", (req, res) => {
   res.send("Story gateway");
@@ -40,6 +63,8 @@ app.post("/answers", async (req: SubmitAnswerRequest, res, next) => {
     next(err);
   }
 });
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
